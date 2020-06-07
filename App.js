a5f1cc02e2bc80615e8b4b019f7657d76d7dc6e4
KevinAmorimD <kevin.amorim02@gmail.com>
2020-06-07 20:28:38 -0300

    aula 10 sqlite

--- App.js
+++ App.js
@@ -4,6 +4,16 @@ import {Provider} from 'react-redux'
 import reduxThunk from 'redux-thunk';
 import UsuariosNavegacao from './navegacao/UsuariosNavegacao';
 import usuariosReducer from './store/usuarios-reducer'
+import {init} from './helpers/db'
+
+init()
+  .then(() => {
+    console.log("Base criada ou já existente!");
+  })
+  .catch((err) => {
+    console.log("Erro ao criar o banco!");
+    console.log(err);
+  })
 
 const rootReducer = combineReducers({
   usuarios: usuariosReducer
@@ -15,5 +25,4 @@ export default function App() {
   return <Provider store={store}>
         <UsuariosNavegacao />
         </Provider>
-
 }
